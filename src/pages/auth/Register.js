ce43ef4aed4a5a69924b06ce17ff8d28891ef3dc
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-08 14:56:32 -0800

    Create pages directory and temporary pages.

---
+++ src/pages/auth/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DocumentTitle from 'react-document-title';
+//import { Link } from 'react-router-dom';
+
+class Register extends Component {
+  render() {
+    return (
+        <DocumentTitle title='Potluck - Register'>
+        <div>Register</div>
+        </DocumentTitle>
+    );
+  }
+}
+
+export default Register;
