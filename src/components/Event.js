5a3b00013268d360dfa3a628840e903fc9a5f169
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-21 13:44:32 -0800

    Added resource route to view events.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Header, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 class Event extends Component {
 
@@ -23,18 +24,20 @@ class Event extends Component {
     }
 
     render() {
-        const {title, location, startTime, endTime} = this.state.data;
+        const { eventId, title, location, startTime, endTime} = this.state.data;
         return (
-            <Card className="event-card">
-                <Header className='event-header' as='h5' attached='top'>{title}</Header>
-                <Segment attached className='event-content'>
-                    Location: {location}    
-                    <br/>
-                    Start Time: {new Date(startTime).toDateString()} 
-                    <br/>
-                    End Time: {new Date(endTime).toDateString()}
-                </Segment>
-            </Card>
+            <Link to={`/dashboard/resource/events/${eventId}`}>
+                <Card className="event-card">
+                    <Header className='event-header' as='h5' attached='top'>{title}</Header>
+                    <Segment attached className='event-content'>
+                        Location: {location}    
+                        <br/>
+                        Start Time: {new Date(startTime).toDateString()} 
+                        <br/>
+                        End Time: {new Date(endTime).toDateString()}
+                    </Segment>
+                </Card>
+            </Link>
         );
   }
 }
---
+++ src/pages/resources/Event.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+//import { Button, Container, Header, Input, Grid } from 'semantic-ui-react'
+
+class Event extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        
+    }
+
+    componentDidMount() {
+        //TODO get resource 
+        //'http://potluckapi.azurewebsites.net/api/';
+    }
+
+
+    render() {
+        return (
+            <div>
+                Event Resource page
+            </div>
+        );
+  }
+}
+export default Event;
