08b42ffc598fef5c8784fd44a199d68e3a470ee0
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-21 13:16:37 -0800

    Remove unused imports.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Navigation from './layout/Navigation';
 import Footer from './layout/Footer';
