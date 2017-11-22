ec776f9a60a447d37c3b0c37b199a84e03ba9ae7
Hansol <leehansol1993@gmail.com>
2017-11-21 17:01:49 -0800

    Closes #16 Edited homepage

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import '../css/foot.css';
 
 class Footer extends Component {
   render() {
     return (
-        <Menu inverted>
+        <Menu inverted className='foot'>
             <Menu.Item>Footer</Menu.Item>
         </Menu>
     );
