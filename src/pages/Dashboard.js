19aa2fd0aab929c43eccb76ce9e2e522338fac0e
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-23 16:02:15 -0800

    Error messages if can't retrieve data. Prevent from freezing if can't retreive data.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Container, Header, Input, Grid, Loader, Dimmer } from 'semantic-ui-react'
+import { Button, Container, Header, Input, Grid, Loader, Dimmer, Message } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 import Collection from '../components/Collection';
 import Event from '../components/Event';
@@ -14,18 +14,20 @@ class Dashboard extends Component {
         super(props);
         this.state = {
             loading: true,
+            error:false,
             Events: [],
             Pledges: [],
             Invitations: []
         };
         this.collect = this.collect.bind(this);
         this.baseUrl = 'http://potluckapi.azurewebsites.net/api/';
-        this.endpoints = ['Events', 'Invitations', 'Pledges'];
+        this.endpoints = ['events', 'invitations', 'pledges'];
     }
 
     componentDidMount() {
         this.collect();
     }
+
 
     /**
      * Collect all user data.
@@ -34,37 +36,53 @@ class Dashboard extends Component {
         let headers = new Headers();
         headers.append('Access-Control-Allow-Origin', '*');
         headers.append('Access-Control-Allow-Credentials', 'true');
+        let auth = "Bearer " + this.props.access; 
+        headers.append('Accept', 'application/json');
+        headers.append("Authorization", auth);
+        
         let options = { header:headers };
         Promise.all(this.endpoints.map((endpoint) => {
             return fetch(this.baseUrl+endpoint, options)
-                    .then(response => { return response.json()})
-                    .then(data => {
-                        return data;
-                    });
+                    .then(response => { 
+                        if(response.ok) {
+                            return response.json()                            
+                        } else {
+                            throw new Error(response.statusText)                        
+                        }
+                    })
+                    .catch(e => { console.log(e); return e})
         }))
         .then(values => {
-            let state = Object.assign({}, this.state);
-            values.map((item, index)=> {
-                let stateKey = this.endpoints[index];
-                state[stateKey] = item;
-            });
-            state.loading = false;
-            setTimeout(()=>{this.setState(state)}, 3000);
+            if(values.find(item => { return item !== undefined })) {
+                let state = Object.assign({}, this.state);
+                values.map((item, index)=> {
+                    let stateKey = this.endpoints[index];
+                    state[stateKey] = item;
+                    return item;
+                });
+                state.loading = false;
+                setTimeout(()=>{this.setState(state)}, 2000);
+            } else {
+                this.setState({
+                    error:true, 
+                    loading:false
+                });
+            }
+            
         })
-        .catch(e => {
-            console.log('Error: ', e)
-        })
-
     }
 
     render() {
-        let {loading} = this.state;
+        let {loading, error} = this.state;
         return (
             <div>
                 <Dimmer active={loading}>
                     <Loader size="massive"/>
                 </Dimmer>
                 <Container>
+                    <Message error={error}
+                        header='There was a problem loading the page. Try refreshing later!'
+                    />
                     <Grid verticalAlign='middle' columns={2} centered padded>
                     <Grid.Row>
                             <Grid.Column mobile={3} computer={3} tablet={3}>
@@ -75,6 +93,7 @@ class Dashboard extends Component {
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
+                    
                 </Container>
                 <br/><br/>
                 <Container>
