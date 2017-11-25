29a27ffecc4efaa217fabd3fdf24699bd299a4fd
Yudhvir Raj <rajyudhvir29@gmail.com>
2017-11-24 23:38:11 -0800

    Register works, posts to server

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
 import DocumentTitle from 'react-document-title';
 import { Form, Grid } from 'semantic-ui-react'
+import axios from 'axios';
 import 'semantic-ui-css/semantic.min.css';
 import '../../css/reglog.css'
 //import { Link } from 'react-router-dom';
 
 class Register extends Component {
+
+    constructor() {
+        super();
+        this.state = {fname: '', lname: '', email: '', password: '', cPassword: ''};
+        this.handleSubmit          = this.handleSubmit.bind(this);
+        this.handleFNameChange     = this.handleFNameChange.bind(this);
+        this.handleLNameChange     = this.handleLNameChange.bind(this);
+        this.handleEmailChange     = this.handleEmailChange.bind(this);
+        this.handlePasswordChange  = this.handlePasswordChange.bind(this);
+        this.handleCPasswordChange = this.handleCPasswordChange.bind(this);
+
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+
+        if (!event.target.checkValidity()) {
+                this.setState({
+                invalid: true,
+                displayErrors: true,
+            });
+            return;
+        }
+
+        const data = {email           : this.state.email,
+                     password        : this.state.password,
+                     confirmpassword : this.state.cPassword };
+      
+        axios({
+            url: "http://potluckapi.azurewebsites.net/api/register",
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify (data)
+        }).then(response => {
+            this.setState({ openModal: true, success: response.status === 201 })
+            console.log ("Works");
+        }).catch(e => {
+            console.log(e)
+            this.setState({ openModal: true, success: false })
+        })
+    }
+
+    handleFNameChange (e) {
+        this.setState({fname: e.target.value});
+    }
+
+    handleLNameChange (e) {
+        this.setState({lname: e.target.value});
+    }
+
+    handleEmailChange (e) {
+        this.setState({email: e.target.value});
+     }
+
+     handlePasswordChange (e) {
+        this.setState({password: e.target.value});
+     }
+
+     handleCPasswordChange (e) {
+        this.setState({cPassword: e.target.value});
+     }
+
   render() {
     return (
         <DocumentTitle title='Potluck - Register'>
@@ -23,12 +87,12 @@ class Register extends Component {
                 </Grid.Row>
                 <Grid.Row >
                     <Grid.Column textAlign="center" computer={6} tablet={10} mobile={16}>
-                        <Form>
-                            <Form.Input label='First Name' type='text' placeholder="First Name"/>
-                            <Form.Input label='Last Name' type='text' placeholder="Last Name"/>
-                            <Form.Input label='E-mail' type='email' placeholder="E-mail"/>
-                            <Form.Input label='Enter Password' type='password' placeholder="Enter password"/>
-                            <Form.Input label='Confirm Password' type='password' placeholder="Confirm password"/>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Input label='First Name' type='text' placeholder="First Name" value={this.state.fname} onChange={this.handleFNameChange} />
+                            <Form.Input label='Last Name' type='text' placeholder="Last Name" value={this.state.lname} onChange={this.handleLNameChange} />
+                            <Form.Input label='E-mail' type='email' placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange} />
+                            <Form.Input label='Enter Password' type='password' placeholder="Enter password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                            <Form.Input label='Confirm Password' type='password' placeholder="Confirm password" value={this.state.cPassword} onChange={this.handleCPasswordChange}/>
                             <label> &nbsp;
                             <button className="confirm-button" name="submit">Sign Up</button>
                             </label>
