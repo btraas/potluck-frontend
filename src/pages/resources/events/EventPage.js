ac349f8b44389a43fe097d67d45907b554a9d8d9
Niko Arellano <narellano@my.bcit.ca>
2017-11-27 01:23:42 -0800

    added api helper. added swagger like api functions

--- src/pages/resources/events/EventPage.js
+++ src/pages/resources/events/EventPage.js
@@ -1,121 +1,193 @@
 import React, { Component } from 'react';
-import { Container, Grid, Dropdown, Progress, Divider  } from 'semantic-ui-react';
+import ApiHelper from '../../../util/ApiHelper'
+import { 
+    Container, 
+    Grid, 
+    Dropdown, 
+    Progress,
+    Segment,
+    Header 
+} from 'semantic-ui-react';
+import { getEventById     } from '../../../api/EventsApi'
+import { getInvitations   } from '../../../api/InvitationsApi'
+import { getItemsForEvent } from '../../../api/ItemsApi'
+import { getPledges }       from '../../../api/PledgesApi' 
+import { getItemCategories } from '../../../api/ItemCategoriesApi'
+import jwt_decode from 'jwt-decode';
 import '../../../css/event.css';
 
 const options = [
-    { key: 1, text: 'Invited', value: 1 },
-    { key: 2, text: 'Not Invited', value: 2 },]
+    { key: 1, text: 'Attending', value: 1 },
+    { key: 2, text: 'Not Attending', value: 2 },]
 
 class EventPage extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
-            loading: true,
-            Event: []
+            loading        : true,
+            event          : {},
+            guests         : [],
+            pledges        : [],
+            items          : [],   
+            itemCategories : [],
         };
-        this.collect = this.collect.bind(this);
-        this.baseUrl = 'http://potluckapi.azurewebsites.net/api/Events/';
+
+        this.api          = new ApiHelper()
+        this.userId       = jwt_decode(sessionStorage.getItem("id_token")).sub
+        this.accessToken  = sessionStorage.getItem("access_token")
     }
   
     componentDidMount() {
-        this.collect();
+        //this.collect();
+    }
+
+    async componentWillMount() {
+        await this._fetchEvent()
+        await this._fetchItemCategories()
+        await this._fetchItems()
+        await this._fetchInvitations()
     }
 
     /**
-     * Collect all user data.
+     * Fetch event data.
      */
-    collect() {
-        let eventid = this.props.match.params.eventId;
-        console.log(this.baseUrl + eventid);
-        let headers = new Headers();
-        headers.append('Access-Control-Allow-Origin', '*');
-        headers.append('Access-Control-Allow-Credentials', 'true');
-        let options = { header:headers };
-        fetch(this.baseUrl+eventid, options)
-            .then(response => { return response.json()})
-            .then(data => {
-                this.setState({
-                    Event:data
-                });
-            })
-            .catch(e => {
-                console.log('Error: ', e)
-            })
+    _fetchEvent = async () => {
+        const { eventId } = this.props.match.params 
 
+        let event = await getEventById(eventId)
+
+        console.log(event)
+
+        return event
+    };
+
+    /**
+     * 
+     */
+    _fetchItemCategories = async () => {
+
+        let itemCategories = await getItemCategories()
+
+        console.log(itemCategories)
+        
+        this.setState({ itemCategories })
+
+        return itemCategories
+    }
+
+    /**
+     * 
+     */
+    _fetchItems = async () => {
+        const { eventId } = this.props.match.params 
+
+        let items = await getItemsForEvent(eventId)
+
+        console.log(items)
+
+        return items
+    }
+
+    _fetchInvitations = async () => {
+
+        let invitations = await getInvitations()
+
+        console.log(invitations)
+
+        return invitations
     }
 
     render() {
-        
-        var organizer = this.state.Event.organizer;
-        console.log(this.state.Event);
+    
         return (
-            <Grid padded id="event-page">
-                <Grid.Row className="event-tiles" centered as={Container}>
-                    <Grid.Column mobile={16} computer={12} tablet={14} textAlign="left" >
-                        <h1>{this.state.Event.title}</h1>
-                        <Dropdown placeholder="Status" options={options} floated='right'/>
-                        <br/><br/>
-                        {/* need to get hosted by from organizer but its says null reference when its not */}
-                        <p>hosted by: /placeholder/ </p>
+            <div>
+            <Segment style={{ backgroundColor : "green" }}>
+                <div style={{ margin : 50 }} >
+                    <Header
+                        as='h1'
+                        content={this.state.event.title}
+                        inverted
+                        style={{ fontSize: '4em', fontWeight: 'normal'}}
+                    />
+                    <Header
+                        as='h2'
+                        content='Hosted by: John Cena'
+                        inverted
+                        style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+                    />
+                </div>
+            </Segment>
+            <Grid container centered id="event-page">
+                <Grid.Row centered as={Container} >
+                    <Grid.Column mobile={16} computer={8} textAlign="center">
+                        <Segment>
+                            {/*<span>Hosting</span>*/}
+                            <Dropdown button fluid placeholder="Status" options={options} style={{ textAlign: "center", backgroundColor : "transparent" }}/>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column mobile={16} computer={8} textAlign="center">
+                        <Segment>
+                            <span>Pledge</span>
+                        </Segment>
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row centered>
-                    <Grid.Column computer={10} mobile={16} tablet={14}>
-                        <Grid>
-                            <Grid.Row centered>
-                                <Grid.Column className="event-tiles" computer={16} tablet={16} mobile={16}  textAlign="left">
-                                <h2>Description</h2>
-                                <br/><br/>
-                                <p>{this.state.Event.description}</p>
-                                </Grid.Column>
-                            </Grid.Row>
+                <Grid.Row centered as={Container}>
+                     <Grid.Column mobile={16} computer={8} textAlign="left">
+                        <Segment>
+                            <h2>Location</h2>
+                            <p>{this.state.event.location}</p>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column mobile={16} computer={8} textAlign="left">
+                        <Segment>
+                            <p><span>Date: </span>{new Date(this.state.event.startTime).toDateString()}</p>
+                            <p><span>Duration: </span>{new Date(this.state.event.startTime).toTimeString()} Until: {new Date(this.state.event.endTime).toTimeString()}</p>
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered as={Container}>
+                    <Grid.Column computer={16} tablet={16} mobile={16}  textAlign="left">
+                        <Segment>
+                            <h2>Description</h2>
                             <br/><br/>
-                            <Grid.Row>
-                                <Grid.Column  className="event-tiles" mobile={16} computer={7} tablet={10} textAlign="left">
-                                <h2>Location</h2>
-                                <p>{this.state.Event.location}</p>
-                                </Grid.Column>
-                                <Divider/>
-                                <Grid.Column floated='right' className="event-tiles" mobile={16} computer={7} tablet={10} textAlign="left">
-                                    <h2>Time: </h2>
-                                    <p>Starts at: {this.state.Event.startTime} Until: {this.state.Event.endTime}</p>
-                                    
-                                </Grid.Column>
-                            </Grid.Row>
-                            <br/><br/><br/>
-                            <Grid.Row >
-                                <Grid.Column className="event-tiles" mobile={16} computer={11} tablet={10} textAlign="left">
-                                    <span>Pledge Status:</span>
-                                    <p className="right-aligned-p">Button</p>
-                                    <br/><br/>
-                                    <p>Food</p>
-                                    <Progress percent={11} />
-                                    <ul>
-                                        <li>
-                                            <p>Celery</p>
-                                            <Progress percent={11} />
-                                        </li>
-                                    </ul>
-                                    <br/><br/>
-                                    <p>Drinks</p>
-                                    <Progress percent={33} />
-                                    <br/><br/>
-                                    <p>Items</p>
-                                    <Progress percent={76} />
-                                </Grid.Column>
-                                <Grid.Column floated='right' className="event-tiles" mobile={16} computer={4} tablet={10} textAlign="left">
-                                    <p>Guests :</p>
-                                    <p>Vincent H Lee (Lord of the potluck)</p>
-                                    <p>Matt Li</p>
-                                    <p>Hansol lee</p>
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
+                            <p>{this.state.event.description}</p>
+                        </Segment>
                     </Grid.Column>
                 </Grid.Row>
-                
+                <Grid.Row centered as={Container}>
+                    <Grid.Column textAlign="left">
+                        <Segment>
+                            <span>Pledge Status:</span>
+                            <p className="right-aligned-p">Button</p>
+                            <p>Food</p>
+                            <Progress percent={11} />
+                            <ul>
+                                <li>
+                                <p>Celery</p>
+                                <Progress percent={11} />
+                                </li>
+                            </ul>
+                            <p>Drinks</p>
+                            <Progress percent={33} />
+                            <p>Items</p>
+                            <Progress percent={76} />
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered as={Container} >
+                    <Grid.Column mobile={16} textAlign="left">
+                        <Segment>
+                            <p>Guests :</p>
+                            <p>Vincent H Lee (Lord of the potluck)</p>
+                            <p>Matt Li</p>
+                            <p>Hansol lee</p>
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
+            </div>
         );
   }
 }
