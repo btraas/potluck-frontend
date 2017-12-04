b1cb2cf118a6a51245b9422bcfb0c8cabe6cf76a
Clint <cougarclint@hotmail.com>
2017-12-04 12:16:18 -0800

    fixed auth layout looking for files didn't exist

--- src/layout/AuthLayout.js
+++ src/layout/AuthLayout.js
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import EventCreate from '../pages/resources/events/EventCreate';
 import EventPage from '../pages/resources/events/EventPage';
-import EventAddGuest from '../pages/resources/events/EventAddGuest';
+//import EventAddGuest from '../pages/resources/events/EventAddGuest';
 import EditEventPledges from '../pages/resources/events/EditEventPledges';
 import YourPledges from '../pages/resources/events/YourPledges';
 
@@ -18,7 +18,7 @@ class AuthLayout extends Component {
             <Route exact path="/dashboard" render={()=><Dashboard {...this.props}/>} />               
             <Route exact path="/dashboard/events/create" render={()=><EventCreate {...this.props}/> }/>
             <Route exact path="/dashboard/events/:eventId" component={EventPage}/>
-            <Route exact path="/dashboard/events/:eventId/addguest" component={EventAddGuest}/>
+            
             <Route exact path='/dashboard/events/:eventId/editpledges' component={EditEventPledges}/>
             <Route exact path='/dashboard/events/:eventId/:userId/yourpledges' component={YourPledges}/>
             <Redirect to="/dashboard"/>    
